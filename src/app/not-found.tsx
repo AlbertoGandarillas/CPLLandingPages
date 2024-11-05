c2f7a172f24c0d72f25d396b4fdcd4a027e126c0
AlbertoGandarillas <albertogandarillas@hotmail.com>
2024-11-05 11:39:33 -0500

    Hide CPL Portal page Modifi notfound page to show goto links

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -16,12 +16,22 @@ export default function NotFoundPage() {
         </CardHeader>
         <CardContent>
           <p className="text-muted-foreground text-center">
-            Sorry, we couldn&apos;t find the page you&apos;re looking for. It might have been removed, had its name changed, or is temporarily
+            Sorry, we couldn&apos;t find the page you&apos;re looking for. It
+            might have been removed, had its name changed, or is temporarily
             unavailable.
           </p>
         </CardContent>
-        <CardFooter className="flex justify-center space-x-4">
-            <Link href="/">Go to Homepage</Link>
+        <CardFooter className="flex flex-col space-x-4">
+          <Link href="/chancellor" className="text-center text-xs text-blue-700" target="_blank">
+            Go to Potential CPL Savings & Preservation of Funds (PoF), 20
+            Year-Impact College Metrics website
+          </Link>
+          <Link
+            href="https://map.rccd.edu/" target="_blank"
+            className="text-center text-xs text-blue-700 mt-4"
+          >
+            Go to CALIFORNIA MAP INITIATIVE website
+          </Link>
         </CardFooter>
       </Card>
     </div>
